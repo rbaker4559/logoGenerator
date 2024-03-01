9b2f1ada413327f967a9a63bbc7776050e4d36fd
rbaker4559 <reginaldsbaker10@icloud.com>
2024-02-29 19:13:09 -0800

    adding generator and save function

--- Assets/index.js
+++ Assets/index.js
@@ -27,37 +27,37 @@ async function promptUser(){
     return userInput;
 }
 
-// function generateLogo(userInput) {
+function generateLogo(userInput) {
 
-//     const {text, color, shape} = userInput;
+    const {text, color, shape} = userInput;
     
-//     let svgContent = `
-//     <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-//     `;
+    let svgContent = `
+    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    `;
 
-//     if(shape === "circle"){
-//         svg += `
-//         <circle cx="150" cy="100" r="80" fill="${color}" />
-//         `;
-//     } else if (shape === "square"){
-//         svg += `
-//         <rect x="50" y="50" width="200" height="100" fill="${color}" />
-//         `;
-//     } else if (shape === "triangle"){
-//         svg += `
-//         <polygon points="150,20 250,180 50,180" fill="${color}" />
-//         `;
-//     }
+    if(shape === "circle"){
+        svg += `
+        <circle cx="150" cy="100" r="80" fill="${color}" />
+        `;
+    } else if (shape === "square"){
+        svg += `
+        <rect x="50" y="50" width="200" height="100" fill="${color}" />
+        `;
+    } else if (shape === "triangle"){
+        svg += `
+        <polygon points="150,20 250,180 50,180" fill="${color}" />
+        `;
+    }
 
-// }
+}
 
-// async function saveAndGenerateLogo() {
-//     try {
-//         const userInput = await promptUser()
-//         const svgContent = generateLogo(userInput);
-//         fs.writeFileSync('logo', svgContent);
-//         console.log("logo saved!");
-//     } catch (error) {
-//         console.log("Error generating logo: ", error);
-//     }
-// }
+async function saveAndGenerateLogo() {
+    try {
+        const userInput = await promptUser()
+        const svgContent = generateLogo(userInput);
+        fs.writeFileSync('logo', svgContent);
+        console.log("logo saved!");
+    } catch (error) {
+        console.log("Error generating logo: ", error);
+    }
+}
